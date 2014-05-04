cf97c390e416de800800297401d7d5ad196013d3
shirsch <sim-94@hotmail.com>
2014-05-05 00:07:40 +0200

    Update BonusMB.java

--- src/BonusMB.java
+++ src/BonusMB.java
@@ -10,7 +10,7 @@ public class BonusMB extends Bonus {
 		boolean multiBalls = false;
 		if (isInside(b.getPosX(), b.getPosY(), b.getLongueur(), b.getHauteur())){
 			multiBalls = true;
-			niveau.balls.add(new Ball(15,15, niveau.raquette.getPosX()+(niveau.raquette.getLongueur()/2), niveau.raquette.getPosY()-12, -3, -1));
+			niveau.balls.add(new Ball(15,15, niveau.raquette.getPosX()+(niveau.raquette.getLongueur()/2), niveau.raquette.getPosY()-12, 0, (float) -4.24));
 //			niveau.balls.add(new Ball(12,12, 50, 380, 3, -1, 0));
 		}
 		return multiBalls;
